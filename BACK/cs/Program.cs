ea8e8f6ec3ecff1e5ee1913bdfefe13c09c3c383
fran253 <123721486+fran253@users.noreply.github.com>
2024-11-12 22:05:51 +0100

    Arreglado Fetch, ahora funciona en cualquier equipo en local

--- BACK/cs/Program.cs
+++ BACK/cs/Program.cs
@@ -2,28 +2,24 @@ using CineApi.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configurar CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("PermitirOrigenLocal", policy =>
     {
-        policy.WithOrigins("http://127.0.0.1:5500") // Permitir el acceso desde tu frontend
+        policy.AllowAnyOrigin() // Permitimos cualquier origen para pruebas /////////////////////////////
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
 });
 
-// Agregar servicios al contenedor
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configuración de CORS en el pipeline de la aplicación
 app.UseCors("PermitirOrigenLocal");
 
-// Configuración del pipeline de peticiones HTTP
 app.UseSwagger();
 app.UseSwaggerUI();
 
@@ -34,4 +30,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 PeliculaController.InicializarDatos();
+
 app.Run();
